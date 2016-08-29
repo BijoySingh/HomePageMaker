9555d9866279d0b6b4980abc1340023e67bb296f
bijoy <bijoysingh693@gmail.com>
2016-08-29 19:24:28 +0100

    Added static root

--- FlatHomePage/settings.py
+++ FlatHomePage/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
 
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
