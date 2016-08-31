5d94b8768115e5acc2394c1a75cf751e196e4889
bijoy <bijoysingh693@gmail.com>
2016-08-31 07:23:13 +0100

    Adding markdown support

--- core/views.py
+++ core/views.py
@@ -3,17 +3,34 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from core.models import Category, BasicItem, Content
+import markdown
 
+def get_markdown_item(item):
+    item.description = markdown.markdown(item.description)
+    return item
 
 def index(request):
     basic = BasicItem.objects.first()
-    categories = Category.objects.order_by('position').all()
-    return render(request, 'index.html', {'categories' : categories, 'basic' : basic})
+    basic.introduction = markdown.markdown(basic.introduction)
 
+    categories_items = Category.objects.order_by('position').all()
+    categories = []
+    for category in categories_items:
+        categories.append(get_markdown_item(category))
+
+    return render(request, 'index.html', {'categories' : categories, 'basic' : basic})
 
 def view_category(request, id):
     basic = BasicItem.objects.first()
+    basic.introduction = markdown.markdown(basic.introduction)
+
     category = Category.objects.filter(id=id).first()
-    contents = Content.objects.filter(category=category).all()
+    category = get_markdown_item(category)
+
+    content_items = Content.objects.filter(category=category).all()
+    contents = []
+    for content in content_items:
+        contents.append(get_markdown_item(content))
+
     return render(request, 'content.html', {'category' : category, 'basic' : basic, 'contents' : contents})
 
