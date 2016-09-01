ade59053be0ff480f3ee113cab9499614c1ac58a
bijoy <bijoysingh693@gmail.com>
2016-09-01 07:04:40 +0100

    Adding IMage in content

--- core/models.py
+++ core/models.py
@@ -37,6 +37,7 @@ class Content(models.Model):
     title = models.CharField(max_length=256, default="")
     description = models.TextField(default="")
     modified_at = models.DateTimeField(auto_now=True, auto_created=True)
+    cover = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return "[" + str(self.category.id) + "][" + str(self.id) + "] " + self.title
